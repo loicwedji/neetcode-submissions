cd7f79e957cc9a8ecdd3913de51a862fa3ec9e5d
Loic Wedji <loicwedji@gmail.com>
2026-07-10 20:44:09 -0400

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> track = new Stack();
+        int[] result = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; i++) {
+            if (i == 0) {
+                track.push(i);
+                continue;
+            }
+            if (temperatures[track.peek()] < temperatures[i]) {
+                while (!track.empty() && temperatures[i] > temperatures[track.peek()]) {
+                    int recent = track.pop();
+                    result[recent] = i - recent;
+                }
+                track.push(i);
+            } else {
+                track.push(i);
+            }
+
+        }
+        return result;
+    }
+}
